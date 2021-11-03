388b03c1df66a1572b59825eee3f63d7ab9e3a3a
Sabrina-EIS <sabrina.shaba@eunoiaits.com>
2021-11-03 03:44:11 -0500

    android daily expense note

---
+++ app/src/main/java/com/example/dailyexpensenote1040/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.dailyexpensenote1040;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+                    Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        // start thread
+        background.start();
+    }
+}
